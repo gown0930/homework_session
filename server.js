7135b2cf628485a865d8965936ae5f3ba5b2faf3
Ubuntu <ubuntu@ip-172-31-32-209.ap-northeast-2.compute.internal>
2023-12-15 08:41:27 +0000

    댓글 수정

--- server.js
+++ server.js
@@ -654,62 +654,206 @@ app.delete("/posts/:idx", (req, res) => {
 //=========댓글=============
 
 //댓글 쓰기
-app.post("/posts/:postidx/comments", (req, res) => {
-   const postId = req.params.postId;
-   const { content } = req.body;
+app.post("/posts/:postIdx/comments", async (req, res) => {
+   try {
+      const user = req.session.user;
 
-   // 데이터베이스에 댓글을 저장하는 로직
+      if (!user) {
+         // 사용자가 로그인되어 있지 않은 경우
+         const result = {
+            success: false,
+            message: "로그인이 필요합니다.",
+         };
+         return res.status(401).send(result);
+      }
 
-   const result = {
-      success: true,
-      message: "댓글이 성공적으로 작성되었습니다.",
-   };
+      const postIdx = req.params.postIdx;
+      const { content } = req.body;
 
-   res.send(result);
+      // 유효성 검사: 댓글 내용이 비어 있는지 확인
+      if (!content) {
+         const result = {
+            success: false,
+            message: "댓글 내용은 필수 입력 항목입니다.",
+         };
+         return res.status(400).send(result);
+      }
+
+      // 데이터베이스에 댓글을 저장하는 로직 쓰기
+
+      const result = {
+         success: true,
+         message: "댓글이 성공적으로 작성되었습니다.",
+      };
+
+      res.status(201).send(result); // 201 Created: 새로운 자원이 성공적으로 생성됨
+   } catch (error) {
+      console.error("댓글 작성 중 에러 발생:", error);
+
+      const result = {
+         success: false,
+         message: "댓글 작성 중 에러가 발생하였습니다.",
+      };
+
+      res.status(500).send(result); // 500 Internal Server Error: 서버 측에서 에러 발생
+   }
 });
 
 //댓글 보기
-app.get("/posts/:postidx/comments", (req, res) => {
-   const postId = req.params.postId;
+app.get("/posts/:postidx/comments", async (req, res) => {
+   try {
+      const postIdx = req.params.postidx;
 
-   // 데이터베이스에서 특정 게시글에 대한 모든 댓글을 가져오는 로직
+      // 데이터베이스에서 특정 게시글에 대한 모든 댓글을 가져오는 로직을 구현
 
-   const comments = [
-      { id: 1, content: "첫 번째 댓글" },
-      { id: 2, content: "두 번째 댓글" },
-      // ... 더 많은 댓글
-   ];
+      const comments = [
+         { idx: 1, content: "첫 번째 댓글" user_idx: 1 },
+         { idx: 2, content: "두 번째 댓글" user_idx: 2 },
+         // ... 더 많은 댓글
+      ];
 
-   res.send(comments);
+      const result = {
+         success: true,
+         message: "댓글을 성공적으로 가져왔습니다.",
+         comments: comments,
+      };
+
+      res.status(200).send(result); // 200 OK: 성공적인 요청
+
+   } catch (error) {
+      console.error("댓글 조회 중 에러 발생:", error);
+
+      const result = {
+         success: false,
+         message: "댓글 조회 중 에러가 발생하였습니다.",
+      };
+
+      res.status(500).send(result); // 500 Internal Server Error: 서버 측에서 에러 발생
+   }
 });
 
 //댓글 수정
-app.put("/posts/:postidx/comments/:commentIdx", (req, res) => {
-   const commentId = req.params.commentId;
-   const { content } = req.body;
+app.put("/posts/:postidx/comments/:commentIdx", async (req, res) => {
+   try {
+      const user = req.session.user;
+      const postIdx = req.params.postidx;
+      const commentIdx = req.params.commentIdx;
+      const { content } = req.body;
 
-   // 데이터베이스에서 특정 댓글을 수정하는 로직
+      if (!user) {
+         // 사용자가 로그인되어 있지 않은 경우
+         const result = {
+            success: false,
+            message: "로그인이 필요합니다.",
+         };
+         return res.status(401).send(result);
+      }
 
-   const result = {
-      success: true,
-      message: "댓글이 성공적으로 수정되었습니다.",
-   };
+      // 유효성 검사: 댓글 내용이 비어 있는지 확인
+      if (!content) {
+         const result = {
+            success: false,
+            message: "댓글 내용은 필수 입력 항목입니다.",
+         };
+         return res.status(400).send(result); // 400 Bad Request: 잘못된 요청
+      }
 
-   res.send(result);
+      // 데이터베이스에서 댓글을 조회
+      const comment = "";
+
+      // 댓글 작성자의 idx와 세션의 idx가 다를 경우
+      if (comment.userIdx !== user.idx) {
+         const result = {
+            success: false,
+            message: "권한이 없습니다.",
+         };
+         return res.status(403).send(result); // 403 Forbidden: 권한이 없음
+      }
+
+      // 댓글이 존재하지 않을 경우
+      if (!comment) {
+         const result = {
+            success: false,
+            message: "댓글을 찾을 수 없습니다.",
+         };
+         return res.status(404).send(result); // 404 Not Found: 댓글을 찾을 수 없음
+      }
+
+      // 데이터베이스에서 댓글을 수정하는 로직 구현하기
+
+      const result = {
+         success: true,
+         message: "댓글이 성공적으로 수정되었습니다.",
+      };
+
+      res.status(200).send(result); // 200 OK: 성공적인 요청
+   } catch (error) {
+      console.error("댓글 수정 중 에러 발생:", error);
+
+      const result = {
+         success: false,
+         message: "댓글 수정 중 에러가 발생하였습니다.",
+      };
+
+      res.status(500).send(result); // 500 Internal Server Error: 서버 측에서 에러 발생
+   }
 });
 
 //댓글 삭제
-app.delete("/posts/:postidx/comments/:commentIdx", (req, res) => {
-   const commentId = req.params.commentId;
+app.delete("/posts/:postidx/comments/:commentIdx", async (req, res) => {
+   try {
+      const user = req.session.user;
+      const postIdx = req.params.postidx;
+      const commentIdx = req.params.commentIdx;
 
-   // 데이터베이스에서 특정 댓글을 삭제하는 로직
+      if (!user) {
+         // 사용자가 로그인되어 있지 않은 경우
+         const result = {
+            success: false,
+            message: "로그인이 필요합니다.",
+         };
+         return res.status(401).send(result);
+      }
 
-   const result = {
-      success: true,
-      message: "댓글이 성공적으로 삭제되었습니다.",
-   };
+      // 데이터베이스에서 댓글 정보 가져오기
+      const comment = "";
 
-   res.send(result);
+      // 댓글이 존재하지 않을 경우
+      if (!comment) {
+         const result = {
+            success: false,
+            message: "댓글을 찾을 수 없습니다.",
+         };
+         return res.status(404).send(result); // 404 Not Found: 댓글을 찾을 수 없음
+      }
+
+      // 댓글 작성자의 idx와 세션의 idx가 다를 경우
+      if (comment.userIdx !== user.idx) {
+         const result = {
+            success: false,
+            message: "권한이 없습니다.",
+         };
+         return res.status(403).send(result); // 403 Forbidden: 권한이 없음
+      }
+
+      // 데이터베이스에서 특정 댓글을 삭제하는 로직을 구현하기
+
+      const result = {
+         success: true,
+         message: "댓글이 성공적으로 삭제되었습니다.",
+      };
+
+      res.status(200).send(result); // 200 OK: 성공적인 요청
+   } catch (error) {
+      console.error("댓글 삭제 중 에러 발생:", error);
+
+      const result = {
+         success: false,
+         message: "댓글 삭제 중 에러가 발생하였습니다.",
+      };
+
+      res.status(500).send(result); // 500 Internal Server Error: 서버 측에서 에러 발생
+   }
 });
 
 
